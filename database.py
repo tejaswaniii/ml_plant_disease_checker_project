c41dafd24fbdabf720e34a8bcc6f5f00f173b901
tejaswaniii <tejaswani_be22cs192@srmcem.ac.ingit config --global user.name>
2025-07-16 09:18:56 +0530

    Deleted one image

--- database.py
+++
@@ -1,5 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient("mongodb://localhost:27017/")  # or use your cloud MongoDB URI
-db = client["plant_disease_db"]
-collection = db["predictions"]
